68be2710080a1b92a426d7e3fa77e1d4df1b7f4a
roxane-hub <marielleroxane037@gmail.com>
2025-11-19 16:55:55 +0200

    commit5

--- src/main/java/com/example/employeemanagementapi/dto/LoginRequest.java
+++ src/main/java/com/example/employeemanagementapi/dto/LoginRequest.java
@@ -11,7 +11,7 @@ public class LoginRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
-    @Schema(example = "jane.doe@gmail.com")
+    @Schema(example = "sara.doe@gmail.com")
     private String email;
 
     @NotBlank(message = "Password is required")
